0c6c6eba3ba47e5577beab37653fcea5142bd093
PixelByteAG <therealpixelbyte@gmail.com>
2018-05-08 22:24:11 -0400

    commit for basic commands and basic check

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const discord = require('discord.js');
 
 //Create an instance of Discord to control the bot
 const bot = new discord.Client();
+//create an array of accepted commands
+var validCommands = ['help', 'fn', 'hello'];
 
 //token
 const token = 'NDQzNTQwMDcyMjA2MzAzMjM3.DdO2ZQ.rN7znpXm9EaQsxRGVimb5mQOfsE';
@@ -15,10 +17,21 @@ bot.on('message', message => {
     //easy check for bot not to confuse itself with itself
     if(message.author.bot) return;
 
-    if(message.content.indexOf('{') === 0){
+    if(message.content.indexOf(';') === 0){
         var text = message.content.substring(1);
+        console.log ('Command Used: ', text);
+        console.log ('User: ', message.author.username);
+        if(validCommands.includes(text)){
+            console.log('Command Successful.');
+            if(text === 'help'){
+                message.reply("if you need help, go fuck yourself.");
+            }
 
-        message.reply("Hi", message.author.name);
+
+        }else{
+            message.reply("not a valid command!", message.author.name);
+        }
+
 
     }
 });
